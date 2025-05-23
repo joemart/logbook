c696854eb78e2121c7d9ec603361b6867a111da7
Joseph Martire <joemart06@gmail.com>
2025-05-23 19:12:06 -0400

    Add debugbar; add mysql connection; add prevent lazyloading

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Models\Car;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
+
+     try {
+        DB::connection()->getPdo();
+        return "Connected successfully to database: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return "Error connecting to database: " . $e->getMessage();
+    }
+
+
+
     return view('welcome');
 });
+

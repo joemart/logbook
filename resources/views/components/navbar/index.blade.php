519366b99ca954d23d9e28082aa9a6e7929ab5b8
Joseph Martire <joemart06@gmail.com>
2025-05-29 11:00:47 -0400

    Add alpine and counter to test it; add navbar

--- resources/views/components/navbar/index.blade.php
+++ resources/views/components/navbar/index.blade.php
@@ -1,9 +1,14 @@
 @vite(['resources/views/components/navbar/_navbar.scss'])
 
-<nav> 
-    <div class="search_dashboard">
+<nav class = "nav"> 
+    <div class="search">
         <img class="magnifying_glass" src="{{asset('images/magnifying-glass.png')}}" alt="">
-        <input class="input_dashboard" type="text">
+        <input class="input_dashboard" type="text" placeholder="Search Callsign or Name">
     </div>
-    
+    <div class="profile">
+        <div x-data= "{count : 0}">
+            <button x-on:click="count++">Increment</button>
+            <span x-text="count"></span>
+        </div>
+    </div>
 </nav>
